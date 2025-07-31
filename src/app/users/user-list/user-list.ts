8e7d8a70db1322a5ab8ef69f88b01e13ad0b1ecc
Vu Xuan Duc <vuxuanduc54@gmail.com>
2025-07-31 17:28:59 +0700

    Update modal confirm

--- src/app/users/user-list/user-list.ts
+++ src/app/users/user-list/user-list.ts
@@ -38,11 +38,22 @@ export class UserList implements OnInit{
         try {
           const res = await this.delete(id);
           if(res.status == 'success') {
-            this.message.success(res.message || 'Xóa thành công');
+            this.modal.success({
+              nzTitle: 'Thành công',
+              nzContent: res.message || 'Xóa thành công'
+            });
+          }else {
+            this.modal.error({
+              nzTitle: 'Thất bại',
+              nzContent: res.message || 'Xóa thất bại'
+            });
           }
         } catch(err) {
           const error = err as Error;
-          this.message.error(error.message || 'Xóa thất bại!');
+          this.modal.error({
+            nzTitle: 'Thất bại',
+            nzContent: error.message || 'Xóa thất bại!'
+          });
         }
       }
     });
@@ -51,8 +62,10 @@ export class UserList implements OnInit{
   delete(id: number): Promise<any> {
     return new Promise((resolve, reject) => {
       this.userService.delete(id).subscribe({
-        next: (res) => {
-          this.users = this.users.filter(u => u.id != id);
+        next: (res: any) => {
+          if(res.status == 'success') {
+            this.users = this.users.filter(u => u.id != id);
+          }
           resolve(res);
         },
         error: (err) => {
@@ -61,4 +74,10 @@ export class UserList implements OnInit{
       })
     })
   }
+  showModal = false;
+
+  deactivate() {
+    this.showModal = false;
+  }
+
 }
